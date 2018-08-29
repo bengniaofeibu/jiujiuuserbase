e0eafb9e012f0528d2f96d0e1d72a663cd95df91
Administrator <tong_hengzhen@163.com>
2018-08-29 17:03:52 +0800

    修改配置

---
+++ src/main/java/enums/ResultEnums.java
@@ -0,0 +1,40 @@
+package enums;
+
+
+import com.jiujiuwisdom.base.BaseEnum;
+
+public enum ResultEnums implements BaseEnum {
+
+ ;
+
+    private Integer code;
+
+    private String msg;
+
+    ResultEnums(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    @Override
+    public Integer getCode() {
+        return null;
+    }
+
+    @Override
+    public void setCode(Integer code) {
+
+    }
+
+    @Override
+    public String getMsg() {
+        return null;
+    }
+
+    @Override
+    public void setMsg(String msg) {
+
+    }
+}
+
+
